79305bf1b9a3df6b6b420079bcd173097d904630
bek_1303 <125305708+BekkAB@users.noreply.github.com>
2025-10-22 17:12:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import json
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
@@ -169,4 +170,5 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
